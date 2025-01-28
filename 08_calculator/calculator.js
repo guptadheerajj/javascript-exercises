2011eadc4c6e6f5d39088e0a254e22b983659f1b
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-01-28 22:33:35 +0530

    feat: solve 08_calculator task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function (...num) {
+	return num.reduce((sum, current) => (sum += current), 0);
 };
 
-const subtract = function() {
-	
+const subtract = function (...num) {
+	return num.reduce((difference, current) => difference - current);
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+	return num.reduce((sum, current) => (sum += current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (num) {
+	return num.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 1;
+  for(let i = num; i > 0; i--) {
+    fact *= i;
+  }
+  return fact
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
