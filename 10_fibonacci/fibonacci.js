6297bdea9ccd236bb52fc42e5692b94541e2b9e1
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-01-31 18:54:12 +0530

    feat: solve 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (nth) {
+	if (typeof nth != Number) {
+		nth = parseInt(nth);
+	}
+	if (nth === 0) return 0;
+	if (nth < 0) return "OOPS";
 
+	let first = 0;
+	let second = 1;
+	for (let i = 2; i <= nth; i++) {
+		[first, second] = [second, first + second];
+	}
+	return second;
 };
 
 // Do not edit below this line
