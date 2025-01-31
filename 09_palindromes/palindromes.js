504a3391ec4eb6a4523cd7906ae7afaf0f6bf8d0
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-01-31 18:36:28 +0530

    feat: solve 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	const validChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+	const cleanString = str
+		.toLowerCase()
+		.split("")
+		.filter((char) => validChars.includes(char))
+		.join("");
+	const reverseCleanString = cleanString.split("").reverse().join("");
 
+	return cleanString === reverseCleanString;
 };
 
 // Do not edit below this line
